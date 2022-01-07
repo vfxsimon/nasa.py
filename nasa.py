378913525706725cf740dc92268aa0f88a0ac6b9
vfxsimon <97272190+vfxsimon@users.noreply.github.com>
2022-01-07 05:44:43 +0100

    Create nasa.py

---
+++ nasa.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+limit = 500
+days = 365
+
+url = f"https://eonet.gsfc.nasa.gov/api/v2.1/events?limit={limit}&days={days}"
+req = requests.get(url)
+events_data = req.json()
+
+with open('data.json', 'w') as f:
+    f.write(json.dumps(events_data, indent=4))
+
+event_list = events_data['events']
+
+for event in event_list:
+    print(event['title'])
